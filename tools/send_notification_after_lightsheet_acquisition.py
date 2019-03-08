4796db8aa00526aece81051e1f9bd0f14c5e78eb
zahramansoor <zahramansoor@github.com>
2019-03-08 09:20:20 -0500

    autopush

--- tools/send_notification_after_lightsheet_acquisition.py
+++ tools/send_notification_after_lightsheet_acquisition.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar  6 18:05:48 2019
+Created on Thu Mar  7 15:00:21 2019
 
-@author: wanglab
+@author: lvbt
 """
 
 import os, time, smtplib, ssl
@@ -12,41 +11,55 @@ from email.mime.text import MIMEText
 
 #this is run on the ls acquisition computer
 
-def send_email(img, receiver_email):
-    file = os.stat(img)
-    mod = time.ctime(file.st_mtime)
+def send_email(img, receiver_email, check):
+    """ sends completion email based on the time when the 50th to last plane was modified """
+    #get stats
+    f = os.stat(os.path.join(img, os.listdir(img)[len(os.listdir(img))-50]))
     
     if receiver_email == "": receiver_email = "zmd@princeton.edu"
+    if check == "": check = 300
+    else: check = int(check)    
     
-    print("\nlast modified time: ", mod)
-    
-    if time.time() - file.st_mtime > 180: #if its not been modified for over 3 minutes 
+    while time.time() - f.st_mtime < 180: #if its not been modified for over 3 minutes 
         
-        smtp_server = "smtp.gmail.com"
-        port = 587  # For starttls
-        sender_email = "pni.lvbt.lightsheet@gmail.com"
-        password = "stephanisawesome"
-        msg = MIMEMultipart()
-        msg["Subject"] = "Light-sheet acquisition complete!"
-        body = "Last modified time of file: {}".format(mod)
-        msg.attach(MIMEText(body, 'plain'))
-        context = ssl.create_default_context()
-        with smtplib.SMTP(smtp_server, port) as server:
-            server.ehlo()  # Can be omitted
-            server.starttls(context=context)
-            server.ehlo()  # Can be omitted
-            server.login(sender_email, password)
-            text = msg.as_string()
-            server.sendmail(sender_email, receiver_email, text)
-        print ("\nsent email :D \n")
+        print("\nlast modified time: ", time.ctime(f.st_mtime))
+        
+        print("\nacquisition not complete, will check again in {} minutes".format(round((check/60), 2)))
+        time.sleep(check) #sleep for 5 minutes
+        
+        #check stats again
+        f = os.stat(os.path.join(img, os.listdir(img)[len(os.listdir(img))-50]))
+        
+    #send email
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
+    sender_email = "pni.lvbt.lightsheet@gmail.com"
+    password = "stephanisawesome"
+    msg = MIMEMultipart()
+    msg["Subject"] = "Light-sheet acquisition complete!"
+    body = "Last modified time of file: {}".format(time.ctime(f.st_mtime))
+    msg.attach(MIMEText(body, 'plain'))
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        text = msg.as_string()
+        server.sendmail(sender_email, receiver_email, text)
+    print ("\nsent email :D \n")
     
-    else:
-        print("acquisition not complete! \n")
 
 if __name__ == "__main__":
     
-    img = "G:\\190307_20170130_tp_bl6_sim_rpv_01_4x_647_008na_1hfds_z7d5um_150msec_10povlp_14-58-10"
-        
-    receiver_email = "zmd@princeton.edu"
+    img = "G:\\190308_20170212_tp_bl6_crii_1000r_02_4x_647_008na_1hfds_z7d5um_75msec_10povlp_08-31-27"
     
-    send_email(img, receiver_email)
+    print("press enter twice to accept all defaults when prompted for input")
+    
+    receiver_email = input("enter receiver email (default is zmd@princeton.edu): ")
+    
+    check = input("seconds after which you would like to check for updates (default is 300 s or 5 min): ")
+    
+    send_email(img, receiver_email, check)
+    
+    
