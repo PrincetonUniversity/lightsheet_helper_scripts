7055319893cb1ac3751c159f4bba785c8ac2593a
zahramansoor <zahramansoor@github.com>
2019-04-18 09:49:52 -0400

    autopush

--- analysis_for_others/20190415_jv_analyze_cell_counts.py
+++ analysis_for_others/20190415_jv_analyze_cell_counts.py
@@ -170,136 +170,133 @@ def generate_paired_statistics(src, csv_pth):
 tdf_dct, sigs = generate_paired_statistics(src, csv_pth)
 ###################################################################DONE##########################################################################################    
 #%%
+#
+##helper functions
+#def correct_cm_to_sturctures(struc):
+#    '''function to correct naming issues
+#    '''
+#    if struc == 'Anterior cingulate area, ventral part, layer 6a': struc = 'Anterior cingulate area, ventral part, 6a'
+#    if struc == 'Anterior cingulate area, ventral part, layer 6b': struc = 'Anterior cingulate area, ventral part, 6b'
+#    if struc == 'Simple lobule': struc = 'Simplex lobule'
+#    if struc == 'Primary somatosensory area, barrel field, layer 5 ': struc = 'Primary somatosensory area, barrel field, layer 5'
+#    return struc
+#
+#def correct_sturctures_to_cm(struc):
+#    '''function to correct naming issues
+#    '''
+#    if struc == 'Anterior cingulate area, ventral part, 6a': struc = 'Anterior cingulate area, ventral part, layer 6a'
+#    if struc == 'Anterior cingulate area, ventral part, 6b': struc = 'Anterior cingulate area, ventral part, layer 6b'
+#    if struc == 'Simplex lobule': struc = 'Simple lobule'
+#    if struc == 'Primary somatosensory area, barrel field, layer 5': struc = 'Primary somatosensory area, barrel field, layer 5 '
+#    return struc  
+#
+#
+#
+#def generate_normalised_structures_list(df_pth, ann_pth, csv_pth):
+#    """
+#    generates counts normalised by volume, correct some errors in look up table
+#    #TODO (zahra): ask tom if this is necessary for PMA
+#    """
+#    #structures
+#    structures = make_structure_objects(df_pth, remove_childless_structures_not_repsented_in_ABA = True, ann_pth=ann_pth)
+#    
+#    #run
+#    df = pd.read_csv(csv_pth)
+#    sois = ['Cortical subplate', 'Cortical plate', 'Cerebral nuclei', 'Thalamus', 'Hypothalamus', 'Midbrain', 'Hindbrain', 'Cerebellum', 'fiber tracts', 'ventricular systems', 'grooves']
+#
+#    #add in progenitor column and add in volumes for area cell counts
+#    vols = pd.read_excel('/jukebox/wang/pisano/Python/lightsheet/supp_files/sample_cell_count_output.xlsx')[['voxels_in_structure', 'name']]
+#    tdf = df.copy()
+#    tdf['progenitor'] = 'empty'
+#    tdf['Volume'] = 0.0
+#    scale_factor = .025 #mm/voxel
+#    
+#    for soi in sois:
+#        soi = [s for s in structures if s.name==soi][0]
+#        print soi.name
+#        progeny = [str(xx.name) for xx in soi.progeny]
+#        for progen in progeny:
+#            progen = correct_sturctures_to_cm(progen)
+#            tdf.loc[tdf['name']==progen,'progenitor']=soi.name
+#            if len(vols[vols['name']==progen]['voxels_in_structure'])>0:
+#                tdf.loc[tdf['name']==progen,'Volume']=vols[vols['name']==progen]['voxels_in_structure'].values[0]*scale_factor
+#    
+#    #drop non progen
+#    tdf = tdf[(tdf['progenitor'] != 'empty') & (tdf['Volume'] != 0.0)].drop(['Unnamed: 0', 'Index'], axis = 1)
+#    
+#    #add normalised column
+#    tdf['count_normalized_by_volume'] = tdf.apply(lambda x:x['Count']/float(x['Volume']), 1)
+#    
+#    #save both as csv and pickle for visualisation
+#    tdf.to_pickle(os.path.join(src, "cell_counts_dataframe_with_progenitors.p"))
+#    tdf.to_csv(os.path.join(src, "cell_counts_dataframe_with_progenitors.csv"))
+#    
+#    print("saved in :{}".format(src))
+#    
+#    return tdf
+#
+##run
+#tdf = generate_normalised_structures_list(df_pth, ann_pth, csv_pth)
+#
+##%%
+#
+#def generate_boxplots(tdf, src):
+#    """
+#    makes representative figures of cell count statistics done previously
+#    inputs:
+#        tdf: cell count dataframe (as dataframe or pickle)
+#        src = folder to save figs
+#    """
+#    if isinstance(tdf, basestring): tdf = pd.read_pickle(tdf)
+#    plt.figure(figsize=(16,8))
+#    g = sns.boxplot(x='progenitor', y='Count', hue='Condition', data=tdf)
+#    g.set_yscale('log')
+#    g.set_title('Cell counts boxplots by progenitor no normalization')
+#    plt.tight_layout()
+#    plt.savefig(os.path.join(src, "cell_counts_boxplots_by_progenitor_no_normalization.pdf"), dpi=300, transparent=True)
+#    plt.close() 
+#
+#    #with cell count norm
+#    plt.figure(figsize=(16,8))
+#    g = sns.boxplot(x='progenitor', y='count_normalized_by_volume', hue='Condition', data=tdf)
+#    g.set_yscale('log')
+#    g.set_title('Cell counts boxplots by progenitor normalized by volume')
+#    plt.tight_layout()
+#    plt.savefig(os.path.join(src, "cell_counts_boxplots_by_progenitor_normalized_by_volume.pdf"), dpi=300, transparent=True)
+#    plt.close()
+#
+#    #total counts per brain
+#    plt.figure(figsize=(16,8))
+#    g=sns.pairplot(tdf, hue='Brain')
+#    g.set(xscale='log', yscale="log")
+#    #plt.tight_layout()
+#    plt.savefig(os.path.join(src, "pairplot_by_brain.pdf"), dpi=300, transparent=True)
+#    plt.close()
+#
+#    plt.figure(figsize=(16,8))
+#    g=sns.pairplot(tdf, hue='Condition')
+#    g.set(xscale='log', yscale="log")
+#    #plt.tight_layout()
+#    plt.savefig(os.path.join(src, "pairplot_by_condition.pdf"), dpi=300, transparent=True)
+#    plt.close()
+#
+#    #sum
+#    #g=tdf.groupby('Condition').sum().plot(kind='bar')
+#    #g.set_yscale('log')
+#    plt.figure(figsize=(16,8))
+#    g = sns.boxplot(x='Brain', y='Count', data=tdf)
+#    g.set_yscale('log')
+#    g.set_xticklabels(g.axes.get_xticklabels(), rotation=30)
+#    g.set_title('Structure total counts per brain')
+#    plt.tight_layout()
+#    plt.savefig(os.path.join(src, "sum_by_brain.pdf"), dpi=300, transparent=True)
+#    plt.close()
+#    
+#    print("saved in : {}".format(src))
+#    
+##run
+#generate_boxplots(tdf, src)
 
-#helper functions
-def correct_cm_to_sturctures(struc):
-    '''function to correct naming issues
-    '''
-    if struc == 'Anterior cingulate area, ventral part, layer 6a': struc = 'Anterior cingulate area, ventral part, 6a'
-    if struc == 'Anterior cingulate area, ventral part, layer 6b': struc = 'Anterior cingulate area, ventral part, 6b'
-    if struc == 'Simple lobule': struc = 'Simplex lobule'
-    if struc == 'Primary somatosensory area, barrel field, layer 5 ': struc = 'Primary somatosensory area, barrel field, layer 5'
-    return struc
-
-def correct_sturctures_to_cm(struc):
-    '''function to correct naming issues
-    '''
-    if struc == 'Anterior cingulate area, ventral part, 6a': struc = 'Anterior cingulate area, ventral part, layer 6a'
-    if struc == 'Anterior cingulate area, ventral part, 6b': struc = 'Anterior cingulate area, ventral part, layer 6b'
-    if struc == 'Simplex lobule': struc = 'Simple lobule'
-    if struc == 'Primary somatosensory area, barrel field, layer 5': struc = 'Primary somatosensory area, barrel field, layer 5 '
-    return struc  
-
-#################################################################################################################################################################
-
-
-def generate_normalised_structures_list(df_pth, ann_pth, csv_pth):
-    """
-    generates counts normalised by volume, correct some errors in look up table
-    #TODO (zahra): ask tom if this is necessary for PMA
-    """
-    #structures
-    structures = make_structure_objects(df_pth, remove_childless_structures_not_repsented_in_ABA = True, ann_pth=ann_pth)
-    
-    #run
-    df = pd.read_csv(csv_pth)
-    sois = ['Cortical subplate', 'Cortical plate', 'Cerebral nuclei', 'Thalamus', 'Hypothalamus', 'Midbrain', 'Hindbrain', 'Cerebellum', 'fiber tracts', 'ventricular systems', 'grooves']
-
-    #add in progenitor column and add in volumes for area cell counts
-    vols = pd.read_excel('/jukebox/wang/pisano/Python/lightsheet/supp_files/sample_cell_count_output.xlsx')[['voxels_in_structure', 'name']]
-    tdf = df.copy()
-    tdf['progenitor'] = 'empty'
-    tdf['Volume'] = 0.0
-    scale_factor = .025 #mm/voxel
-    
-    for soi in sois:
-        soi = [s for s in structures if s.name==soi][0]
-        print soi.name
-        progeny = [str(xx.name) for xx in soi.progeny]
-        for progen in progeny:
-            progen = correct_sturctures_to_cm(progen)
-            tdf.loc[tdf['name']==progen,'progenitor']=soi.name
-            if len(vols[vols['name']==progen]['voxels_in_structure'])>0:
-                tdf.loc[tdf['name']==progen,'Volume']=vols[vols['name']==progen]['voxels_in_structure'].values[0]*scale_factor
-    
-    #drop non progen
-    tdf = tdf[(tdf['progenitor'] != 'empty') & (tdf['Volume'] != 0.0)].drop(['Unnamed: 0', 'Index'], axis = 1)
-    
-    #add normalised column
-    tdf['count_normalized_by_volume'] = tdf.apply(lambda x:x['Count']/float(x['Volume']), 1)
-    
-    #save both as csv and pickle for visualisation
-    tdf.to_pickle(os.path.join(src, "cell_counts_dataframe_with_progenitors.p"))
-    tdf.to_csv(os.path.join(src, "cell_counts_dataframe_with_progenitors.csv"))
-    
-    print("saved in :{}".format(src))
-    
-    return tdf
-
-#run
-tdf = generate_normalised_structures_list(df_pth, ann_pth, csv_pth)
-
-###################################################################DONE##########################################################################################    
-#%%
-
-def generate_boxplots(tdf, src):
-    """
-    makes representative figures of cell count statistics done previously
-    inputs:
-        tdf: cell count dataframe (as dataframe or pickle)
-        src = folder to save figs
-    """
-    if isinstance(tdf, basestring): tdf = pd.read_pickle(tdf)
-    plt.figure(figsize=(16,8))
-    g = sns.boxplot(x='progenitor', y='Count', hue='Condition', data=tdf)
-    g.set_yscale('log')
-    g.set_title('Cell counts boxplots by progenitor no normalization')
-    plt.tight_layout()
-    plt.savefig(os.path.join(src, "cell_counts_boxplots_by_progenitor_no_normalization.pdf"), dpi=300, transparent=True)
-    plt.close() 
-
-    #with cell count norm
-    plt.figure(figsize=(16,8))
-    g = sns.boxplot(x='progenitor', y='count_normalized_by_volume', hue='Condition', data=tdf)
-    g.set_yscale('log')
-    g.set_title('Cell counts boxplots by progenitor normalized by volume')
-    plt.tight_layout()
-    plt.savefig(os.path.join(src, "cell_counts_boxplots_by_progenitor_normalized_by_volume.pdf"), dpi=300, transparent=True)
-    plt.close()
-
-    #total counts per brain
-    plt.figure(figsize=(16,8))
-    g=sns.pairplot(tdf, hue='Brain')
-    g.set(xscale='log', yscale="log")
-    #plt.tight_layout()
-    plt.savefig(os.path.join(src, "pairplot_by_brain.pdf"), dpi=300, transparent=True)
-    plt.close()
-
-    plt.figure(figsize=(16,8))
-    g=sns.pairplot(tdf, hue='Condition')
-    g.set(xscale='log', yscale="log")
-    #plt.tight_layout()
-    plt.savefig(os.path.join(src, "pairplot_by_condition.pdf"), dpi=300, transparent=True)
-    plt.close()
-
-    #sum
-    #g=tdf.groupby('Condition').sum().plot(kind='bar')
-    #g.set_yscale('log')
-    plt.figure(figsize=(16,8))
-    g = sns.boxplot(x='Brain', y='Count', data=tdf)
-    g.set_yscale('log')
-    g.set_xticklabels(g.axes.get_xticklabels(), rotation=30)
-    g.set_title('Structure total counts per brain')
-    plt.tight_layout()
-    plt.savefig(os.path.join(src, "sum_by_brain.pdf"), dpi=300, transparent=True)
-    plt.close()
-    
-    print("saved in : {}".format(src))
-    
-#run
-generate_boxplots(tdf, src)
-
-###################################################################DONE##########################################################################################        
 #%% #look at cross sections
 
 def check_registration_cross_sections(out):
@@ -450,8 +447,8 @@ def generate_percent_counts_and_density_per_region(src, csv_pth):
     df_percent = pd.concat(percents)
     
     df["percent_per_total_counts (%)"] = df_percent
-    df["volume"] = df[df.voxels_in_structure > 0].apply(lambda x: x.voxels_in_structure*scale, 1)
-    df["density (cells/mm3)"] = df[df.voxels_in_structure > 0].apply(lambda x:x.counts/(float(x.voxels_in_structure*scale)), 1)
+    df["volume"] = df[df.voxels_in_structure > 0].apply(lambda x: x.voxels_in_structure*(scale**3), 1)
+    df["density (cells/mm3)"] = df[df.voxels_in_structure > 0].apply(lambda x:x.counts/(float(x.voxels_in_structure*(scale**3))), 1)
     #export
     df.to_csv(os.path.join(src, "cell_counts_dataframe_w_percents_density.csv"))
     
