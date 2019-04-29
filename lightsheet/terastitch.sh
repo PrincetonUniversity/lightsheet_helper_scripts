3b19c663884da7672b8a3ac21741f4be1f55e050
zahramansoor <zahramansoor@github.com>
2019-04-29 18:40:35 -0400

    autopush

---
+++ lightsheet/terastitch.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+#
+#SBATCH -p all                # partition (queue)
+#SBATCH -c 6                      # number of cores
+#SBATCH -t 240                 # time (minutes)
+#SBATCH -o /scratch/zmd/logs/step1_terastitcher_%j.out        # STDOUT #add _%a to see each array job
+#SBATCH -e /scratch/zmd/logs/step1_terastitcher_%j.err        # STDERR #add _%a to see each array job
+#SBATCH --contiguous #used to try and get cpu mem to be contigous
+#SBATCH --mem 80000
+
+echo "In the directory: `pwd` "
+echo "As the user: `whoami` "
+echo "on host: `hostname` "
+
+cat /proc/$$/status | grep Cpus_allowed_list
+
+module load anacondapy/2.7
+. activate lightsheet
+
+xvfb-run python terastitch.py  #process zplns, check that 1000 > zplns/slurmfactor
+
+# HOW TO USE:
+# sbatch --array=0-20 sub_arrayjob.sh
+#xvfb-run --auto-servernum --server-num=1
