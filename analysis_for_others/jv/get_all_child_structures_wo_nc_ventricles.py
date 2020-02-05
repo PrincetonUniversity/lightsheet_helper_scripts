434278f57dd306bda134d47ce5e33ef7788fcf20
zahramansoor <zahramansoor@github.com>
2020-02-05 16:45:31 -0500

    new analysis for jess

--- analysis_for_others/jv/get_all_child_structures_wo_nc_ventricles.py
+++ analysis_for_others/jv/get_all_child_structures_wo_nc_ventricles.py
@@ -10,14 +10,15 @@ import pandas as pd, os
 os.chdir("/jukebox/wang/zahra/python/lightsheet_py3")
 from tools.analysis.network_analysis import make_structure_objects
 
-pth = "/jukebox/LightSheetTransfer/atlas/ls_id_table_w_voxelcounts.xlsx"
+pth = "/home/wanglab/wang/zahra/registration_error_pma/structures_zeroed_after_80um_erosion_pma_annotation_2018.csv"
+df_pth = "/home/wanglab/LightSheetTransfer/atlas/ls_id_table_w_voxelcounts.xlsx"
 dst = "/jukebox/wang/zahra/registration_error_pma"
 
 #make structures to traverse hierarchy
-structures = make_structure_objects(pth)
+structures = make_structure_objects(df_pth)
 
 #read main annotation LUT
-anndf = pd.read_excel(pth)
+anndf = pd.read_csv(pth)
 
 #make new df to only save child structures that don't belong to NC, white matter, or ventricles
 sois = ["Isocortex", "ventricular systems", "fiber tracts", "grooves"]
@@ -29,5 +30,5 @@ for soi in sois:
         anndf = anndf[anndf.name != progen]
         
 #format    
-anndf = anndf.drop(columns = ["Unnamed: 0", "cell_count"])
-anndf.to_csv(os.path.join(dst, "LUT_child_structures_wo_NC_ventr_fiber_tracts.csv"))
+anndf = anndf.drop(columns = ["Unnamed: 0"])
+anndf.to_csv(os.path.join(dst, "structures_zeroed_after_80um_erosion_pma_annotation_2018_nc_vent_removed.csv"))
