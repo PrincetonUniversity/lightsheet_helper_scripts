00e0277078d2b2995aefe1804586968d0903c1b6
zahramansoor <zahramansoor@github.com>
2020-04-14 10:38:01 -0400

    autopush

--- analysis_for_others/jv/20200205_erosion_dilation_atlas_analysis.py
+++ analysis_for_others/jv/20200205_erosion_dilation_atlas_analysis.py
@@ -6,40 +6,50 @@ Created on Wed Feb  5 10:15:36 2020
 @author: wanglab
 """
 
-
-import numpy as np, os, matplotlib.pyplot as plt, tifffile, pandas as pd, seaborn as sns
+import numpy as np, os, matplotlib.pyplot as plt, tifffile
+import pandas as pd, SimpleITK as sitk, seaborn as sns
 from collections import Counter
-os.chdir("/jukebox/wang/zahra/python/lightsheet_py3")
-from tools.analysis.network_analysis import make_structure_objects
 
 #set appropriate pths
-erode_pth = "/jukebox/wang/zahra/registration_error_pma/annotation_pma_2018_20um_sagittal_erode_80um.tif"
-dilate_pth = "/jukebox/wang/zahra/registration_error_pma/dilated_atlases"
+src = "/jukebox/wang/zahra/registration_error_pma/"
+erode_pth = os.path.join(src, "annotation_pma_2018_20um_sagittal_erode_80um.tif")
+dilate_pth = os.path.join(src, "dilated_atlases")
 
-fig_dst = "/home/wanglab/Desktop/zahra"
+fig_dst = "/jukebox/wang/zahra/registration_error_pma/"
 df_pth = "/jukebox/LightSheetTransfer/atlas/ls_id_table_w_voxelcounts.xlsx"
 ann_pth = "/jukebox/LightSheetTransfer/atlas/annotation_sagittal_atlas_20um_iso.tif"
 
 #%%
 #read vols
-ann = tifffile.imread(ann_pth)
+ann = sitk.GetArrayFromImage(sitk.ReadImage(ann_pth))
 df = pd.read_excel(df_pth)
 er_ann = tifffile.imread(erode_pth)
 dl_anns = [os.path.join(dilate_pth, xx) for xx in os.listdir(dilate_pth)]
 
-org_iids = np.unique(ann)[1:] #excluding 0, basic cell groups
+org_iids = np.unique(ann)[1:] #excluding 0
 er_iids = np.unique(er_ann)[1:]
 
 missing = [iid for iid in org_iids if iid not in er_iids]
 
-missing_struct_names = [nm for nm in df.name.values if df.loc[df.name == nm, "id"].values[0] in missing][1:] #excluding root
+missing_struct_names = [nm for nm in df.name.values if df.loc[df.name == nm, "id"].values[0] in missing] #excluding root
 missing_struct_voxels = [df.loc[df.name == nm, "voxels_in_structure"].values[0] for nm in missing_struct_names]
+#replace id column that matches to names
+missing_struct_ids = [df.loc[df.name == nm, "id"].values[0] for nm in missing_struct_names]
 
-#make structures to traverse hierarchy
-structures = make_structure_objects(df_pth)
+#get parent names
+missing_struct_parents = [df.loc[df["id"] == iid, "parent_name"].values[0]
+                          for iid in missing_struct_ids]
 
-missing_struct_parents = [obj.parent[1] for obj in structures if obj.name in missing_struct_names]
+#export missing structures name, id, and total voxel count
 
+dataf = pd.DataFrame()
+dataf["name"] = missing_struct_names
+dataf["id"] = missing_struct_ids
+dataf["parent_name"] = missing_struct_parents
+dataf["voxels_in_structure"] = missing_struct_voxels
+
+dataf.to_csv(os.path.join(src, "structures_zeroed_after_80um_erosion_pma_annotation_2018.csv"))
+print("\ncsv exported!\n")
 #%%
 #plot results
 #barplot of areas most represented by missing structures
@@ -94,20 +104,9 @@ df["type"] = ["eroded"]*len(missing_struct_voxels) + ["original"]*len(all_struct
 
 plt.figure()
 sns.stripplot(x = "num_voxels", y = "type", data = df,  color = "crimson", orient = "h")
-sns.boxplot(x = "num_voxels", y = "type", data = df, orient = "h", showfliers=False, showcaps=False, 
+sns.boxplot(x = "num_voxels", y = "type", data = df, orient = "h", showfliers=False, showcaps=False,
             boxprops={'facecolor':'None'})
 plt.xlim([0, 200000])
 plt.xlabel("Total number of voxels in structure")
 plt.ylabel("Structures 'zero'ed' out vs. all original structures")
 plt.savefig(os.path.join(fig_dst, "boxplot_total_voxels_org_vs_eroded.pdf"), bbox_inches = "tight")
-
-#%%
-#export missing structures name, id, and total voxel count
-
-dataf = pd.DataFrame()
-dataf["name"] = missing_struct_names
-dataf["id"] = missing[1:]
-dataf["parent_name"] = missing_struct_parents
-dataf["voxels_in_structure"] = missing_struct_voxels
-
-dataf.to_csv("/jukebox/wang/zahra/registration_error_pma/structures_zeroed_after_80um_erosion_pma_annotation_2018.csv")
