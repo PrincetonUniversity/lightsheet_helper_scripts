7b271682d0c327c59c63f868f9cfcdc5a55693f0
zahramansoor <zahramansoor@github.com>
2020-05-05 12:43:49 -0400

    autopush

--- clearmap/check_cell_center_mapping.py
+++ clearmap/check_cell_center_mapping.py
@@ -49,6 +49,7 @@ def check_cell_center_to_fullsizedata(brain, zstart, zstop, dst, resizef, annota
         dst = path of tif stack to save
     NOTE: 20+ PLANES CAN OVERLOAD MEMORY
     """
+    print("\n"+os.path.basename(brain)+"\n")
     start = time.time()
 
     #doing things without loading parameter dict
@@ -59,24 +60,18 @@ def check_cell_center_to_fullsizedata(brain, zstart, zstop, dst, resizef, annota
 
     #not the greatest way to do things, but works
     src = [os.path.join(cellch, xx) for xx in os.listdir(cellch) if xx[-3:] == "tif" and int(xx[-7:-4]) in range(zstart, zstop)]; src.sort()
-
     raw = np.zeros((len(src), tifffile.imread(src[0]).shape[0], tifffile.imread(src[0]).shape[1]))
-
     for i in range(len(src)):
         raw[i, :, :] = tifffile.imread(src[i])
-
     pth = os.path.join(brain, "clearmap_cluster_output/cells.npy")
     cells = np.load(pth) #this is in x,y,z!!!!!!!!!!!!!!!
-
     cells = cells[(cells[:, 2] >= zstart) & (cells[:, 2] <= zstop-1)] #-1 to account for range
-
-    cell_centers = np.zeros(raw.shape)
-
+    cell_centers = np.zeros(raw.shape
+    print("\n**********making cell center map**********\n")
     for i, r in enumerate(cells):
         cell_centers[r[2]-zstart, r[1]-1:r[1]+1, r[0]-1:r[0]+1] = 50000
 
     rbg = np.stack([raw.astype("uint16"), cell_centers.astype("uint16"), np.zeros_like(raw)], -1)
-    
     #add the annotation volume transformed-to-raw-space as the 'blue' channel in the RGB stack (3 color overlay)
     if annotation:
         src = [os.path.join(annotation, xx) for xx in os.listdir(annotation) if xx[-3:] == "tif" and int(xx[-7:-4]) in range(zstart, zstop)]; src.sort()
@@ -86,10 +81,11 @@ def check_cell_center_to_fullsizedata(brain, zstart, zstop, dst, resizef, annota
             ann_raw[i, :, :] = tifffile.imread(src[i])
         #add 'blue' channel to rbg stack
         rbg = np.stack([raw.astype("uint16"), cell_centers.astype("uint16"), ann_raw("uint16")], -1)
-        
+
     #resize the whole stack
+    print("**********resizing volume**********\n")
     resize_merged_stack(rbg, os.path.join(dst, "{}_raw_cell_centers_resizedfactor{}_z{}-{}.tif".format(os.path.basename(brain),
-                                          resizef, zstart, zstop)), "uint16", resizef)
+        resizef, zstart, zstop)), "uint16", resizef)
 
     print("took %0.1f seconds to make merged maps for %s" % ((time.time()-start), brain))
 
@@ -120,13 +116,13 @@ def check_cell_center_to_resampled(brain, zstart, zstop, dst, annotation=False):
         cell_centers[r[2]-zstart, r[1]-1:r[1]+1, r[0]-1:r[0]+1] = 50000
 
     rbg = np.stack([raw.astype("uint16"), cell_centers.astype("uint16"), np.zeros_like(raw)], -1)
-    
+
     #add the annotation volume transformed-to-raw-space as the 'blue' channel in the RGB stack (3 color overlay)
     if annotation:
         #add 'blue' channel to rbg stack
         ann = tifffile.imread(annotation)
         rbg = np.stack([raw.astype("uint16"), cell_centers.astype("uint16"), ann("uint16")], -1)
-        
+
     resize_merged_stack(rbg, os.path.join(dst, "{}_raw_cell_centers_resized_z{}-{}.tif".format(os.path.basename(brain),
                                           zstart, zstop)), "uint16", 6)
 
@@ -134,12 +130,12 @@ def check_cell_center_to_resampled(brain, zstart, zstop, dst, annotation=False):
 
 
 if __name__ == "__main__":
-    src = "/jukebox/wang/Jess/lightsheet_output/201904_ymaze_cfos/processed/"
-    dst = "/jukebox/wang/Jess/lightsheet_output/201904_ymaze_cfos/qc"
+    src = "/jukebox/LightSheetData/rat-brody/processed/201910_tracing/clearmap"
+    dst = "/jukebox/LightSheetData/rat-brody/processed/201910_tracing/qc"
     if not os.path.exists(dst): os.mkdir(dst)
-    zstart = 300; zstop = 320 #z-plane #'s you want to visualize at a time, 250-400 is probably ideal for thalamus
-    resizef = 1 #factor by which to downsize raw and cell center overlay, keep 1 if you do not want to downsize
-    ids = ["an20"]
+    zstart = 300; zstop = 310 #z-plane #'s you want to visualize at a time, 250-400 is probably ideal for thalamus
+    resizef = 2 #factor by which to downsize raw and cell center overlay, keep 1 if you do not want to downsize
+    ids = ["z266", "z267", "z268", "z269"]
 
     for i in ids:
         brain = os.path.join(src, i)
