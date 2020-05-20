288548806933f13848236ffe3246b66f2c806d92
zahramansoor <zahramansoor@github.com>
2020-05-20 11:29:45 -0400

    autopush

--- projects/rat_atlas/step0_modify_atlas_for_lightsheet_registration.py
+++ projects/rat_atlas/step0_modify_atlas_for_lightsheet_registration.py
@@ -17,23 +17,32 @@ if __name__ == "__main__":
     zrng = (62, 364)
 
     #load in wh colored atlas and reorient
-    ann = np.fliplr(sitk.GetArrayFromImage(sitk.ReadImage("/jukebox/LightSheetData/brodyatlas/atlas/original/WHS_SD_rat_atlas_v3.nii")))
-    nann="/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_atlas_v3_anterior_up_DV.tif"
+    orgn = "/jukebox/LightSheetData/brodyatlas/atlas/original/WHS_SD_rat_atlas_v3.nii"
+    ann = np.fliplr(sitk.GetArrayFromImage(sitk.ReadImage(orgn)))
     #here we are reorienting to A-P, D-V orientation (how our images are taken) and THEN CROP
     ann = ann[::-1][zrng[0]:zrng[1],yrng[0]:yrng[1],xrng[0]:xrng[1]]
+    #set destination
+    nann = "/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_atlas_v3_anterior_up_DV.tif"
     tifffile.imsave(nann, ann)
 
-    auto = np.fliplr(sitk.GetArrayFromImage(sitk.ReadImage("/jukebox/LightSheetData/brodyatlas/atlas/original/WHS_SD_rat_T2star_v1.01.nii")))
-    nauto = "/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_T2star_v1.01_anterior_up_DV.tif"
+    #load in composite brain
+    org = "/jukebox/LightSheetData/brodyatlas/atlas/original/WHS_SD_rat_T2star_v1.01.nii"
+    #apply as above
+    auto = np.fliplr(sitk.GetArrayFromImage(sitk.ReadImage(org)))
     auto = auto[::-1][zrng[0]:zrng[1],yrng[0]:yrng[1],xrng[0]:xrng[1]]
+    #set dsetination
+    nauto = "/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_T2star_v1.01_anterior_up_DV.tif"
     tifffile.imsave(nauto, auto)
 
     #remove skull in atlas using mask
     from skimage.external import tifffile
-    mask = tifffile.imread("/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_atlas_v3_anterior_up_DV.tif")
-    atlas = tifffile.imread("/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_T2star_v1.01_anterior_up_DV.tif")
+    nann = "/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_atlas_v3_anterior_up_DV.tif"
+    nauto = "/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_T2star_v1.01_anterior_up_DV.tif"
+    mask = tifffile.imread(nann)
+    atlas = tifffile.imread(nauto)
     atlas[mask==0] = 0
-    tifffile.imsave("/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_T2star_v1.01_anterior_up_skullremoved_DV.tif", atlas)
+    skrm = "/jukebox/LightSheetData/brodyatlas/atlas/modified/WHS_SD_rat_T2star_v1.01_anterior_up_skullremoved_DV.tif"
+    tifffile.imsave(skrm, atlas)
 
     #make df for labels
     ndf = pd.DataFrame(columns=["name", "id"])
@@ -49,7 +58,7 @@ if __name__ == "__main__":
     for i in range(len(lines)):
         print ("{} of {}".format(i, len(lines)))
         line = lines[i]
-        vals, structure, empty = line.split(""")
+        vals, structure, empty = line.split("'")
         idx, r, g, b, a, vis, msh = vals.split(); r=int(r); g=int(g); b=int(b)
         ndf.loc[i] = [structure, idx]
 
