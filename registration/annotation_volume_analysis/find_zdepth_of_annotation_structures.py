e6464287edc12ceb77017253d59b4eba9d3236c1
zahramansoor <zahramansoor@github.com>
2020-05-20 12:44:31 -0400

    autopush

--- registration/annotation_volume_analysis/find_zdepth_of_annotation_structures.py
+++ registration/annotation_volume_analysis/find_zdepth_of_annotation_structures.py
@@ -6,6 +6,7 @@ Created on Mon May 11 11:39:05 2020
 """
 
 import os, tifffile, numpy as np, pandas as pd, json
+from scipy.ndimage import center_of_mass
 
 def get_progeny(dic,parent_structure,progeny_list):
     """ 
@@ -39,9 +40,9 @@ def get_progeny(dic,parent_structure,progeny_list):
 if __name__ == "__main__":
 
     #reading paths
-    ann_pth = "/jukebox/LightSheetTransfer/atlas/annotation_sagittal_atlas_20um_iso.tif"
-    df_pth = "/jukebox/LightSheetTransfer/atlas/ls_id_table_w_voxelcounts.xlsx"
-    dst = "/jukebox/LightSheetTransfer/atlas"
+    ann_pth = "/jukebox/LightSheetTransfer/atlas/allen_atlas/annotation_2017_25um_sagittal_forDVscans.tif"
+    df_pth = "/jukebox/LightSheetTransfer/atlas/allen_atlas/allen_id_table_w_voxel_counts.xlsx"
+    dst = "/jukebox/LightSheetTransfer/atlas/allen_atlas"
     ontology_file = "/jukebox/LightSheetTransfer/atlas/allen_atlas/allen.json"
     
     #get progeny of all large structures
@@ -53,7 +54,10 @@ if __name__ == "__main__":
     df = pd.read_excel(df_pth)
     
     #init z-depth column in dataframe
-    df["dorsal-ventral_coordinate"] = [np.nan]*len(df)
+    df["dorsal-ventral_depth_median"] = [np.nan]*len(df)
+    df["dorsal-ventral_depth_min"] = [np.nan]*len(df)
+    df["dorsal-ventral_depth_max"] = [np.nan]*len(df)
+    df["dorsal-ventral_depth_center_of_mass"] = [np.nan]*len(df)
     #get all structure ids
     iids = np.unique(ann).astype("float32")
     
@@ -63,13 +67,23 @@ if __name__ == "__main__":
         zid, yid, xid = np.where(ann == iid)
         #take the median of the z-coordinate to get 'center' in z
         x_median = np.median(xid)
+        x_min = np.min(xid)
+        x_max = np.max(xid)
+        #find the center of mass
+        ann_iid = np.zeros_like(ann)
+        ann_iid[zid,yid,xid] = 1
+        zcm, ycm, xcm = center_of_mass(ann_iid)
         if i%50==0:
             print("******%s******\n" % iid)
             print("******median dorsal-ventral_coordinate is %s******\n" % x_median)
-        df.loc[df.id == iid, "dorsal-ventral_coordinate"] = x_median
+            print("******center of mass is %s******\n" % xcm)
+        df.loc[df.id == iid, "dorsal-ventral_depth_median"] = x_median
+        df.loc[df.id == iid, "dorsal-ventral_depth_min"] = x_min
+        df.loc[df.id == iid, "dorsal-ventral_depth_max"] = x_max
+        df.loc[df.id == iid, "dorsal-ventral_depth_center_of_mass"] = np.round(xcm, 1)
     
     print("******saving to dataframe...******\n")
-    df.to_csv(os.path.join(dst, "ls_id_table_w_voxelcounts_n_DVcoord.csv"))
+    df.to_excel(os.path.join(dst, "allen_id_table_w_voxel_counts_n_DVcoord.xlsx"))
     
     #%%
     
