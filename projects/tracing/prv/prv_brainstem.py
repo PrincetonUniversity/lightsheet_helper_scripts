231b1c72ddc9c217cc02457bc86962dc25abdf42
zahramansoor <zahramansoor@github.com>
2020-06-11 12:25:00 -0400

    autopush

--- projects/tracing/prv/prv_brainstem.py
+++ projects/tracing/prv/prv_brainstem.py
@@ -69,15 +69,16 @@ def get_progeny(dic,parent_structure,progeny_list):
 with open(ontology_file) as json_file:
     ontology_dict = json.load(json_file)
 
-sois = ["Cerebellar nuclei",
-        "Inferior olivary complex", "Superior colliculus, sensory related",
-        "Superior colliculus, motor related", "Pontine gray",
+sois = ["Cerebellar nuclei", "Vestibular nuclei", "Pontine gray",
         "Tegmental reticular nucleus", "Lateral reticular nucleus",
-        "External cuneate nucleus", "Vestibular nuclei",
-        "Principal sensory nucleus of the trigeminal",
+        "Superior colliculus, motor related", 
+        "Superior colliculus, sensory related",
         "Spinal nucleus of the trigeminal, caudal part",
         "Spinal nucleus of the trigeminal, interpolar part",
-        "Spinal nucleus of the trigeminal, oral part"]
+        "Spinal nucleus of the trigeminal, oral part",
+        "Principal sensory nucleus of the trigeminal",
+        "External cuneate nucleus", "Gracile nucleus", "Cuneate nucleus"
+        ]
 
 #first calculate counts across entire region
 counts_per_struct = []
@@ -115,22 +116,25 @@ density = np.nan_to_num(np.array([xx/(vol[i]*(scale_factor**3)) for i, xx in enu
 #%%
 #display
 #set colorbar features 
-maxdensity = 450
-yaxis = np.flipud(sois)
+maxdensity = 500
+sort_descending = False
 
 #make density map like the h129 dataset
 ## display
-fig, axes = plt.subplots(ncols = 1, nrows = 2, figsize = (8,5), sharex = True, gridspec_kw = {"wspace":0, "hspace":0,
-                         "height_ratios": [3,5]})
+fig, axes = plt.subplots(ncols = 1, nrows = 2, figsize = (8,6), sharex = True, gridspec_kw = {"wspace":0, "hspace":0,
+                         "height_ratios": [2.5,5]})
 
 
 #sort inj fractions by primary lob
 sort_density = [density[np.where(primary_pool == idx)[0]] for idx in np.unique(primary_pool)]
 sort_density = np.array(list(itertools.chain.from_iterable(sort_density)))
-#now sort sois by # of neurons/density
-sort_sois = np.array(sois)[np.argsort(np.median(sort_density,axis=0))]
-sort_density = sort_density.T[np.argsort(np.median(sort_density,axis=0))][::-1].T
-yaxis = sort_sois
+if sort_descending == True:
+    #now sort sois by # of neurons/density
+    sort_sois = np.array(sois)[np.argsort(np.median(sort_density,axis=0))]
+    sort_density = sort_density.T[np.argsort(np.median(sort_density,axis=0))][::-1].T
+    yaxis = sort_sois
+else: 
+    yaxis = np.flipud(sois)
 sort_brains = [np.asarray(brains)[np.where(primary_pool == idx)[0]] for idx in np.unique(primary_pool)]
 sort_inj = [frac_of_inj_pool[np.where(primary_pool == idx)[0]] for idx in np.unique(primary_pool)]
 sort_brains = list(itertools.chain.from_iterable(sort_brains))
@@ -187,22 +191,25 @@ plt.savefig(os.path.join(dst, "prv_density_brainstem.jpg"), bbox_inches = "tight
 #%%
 #display - just counts
 #set colorbar features 
-maxdensity = 800
+maxdensity = 600
 yaxis = np.flipud(sois)
 
 #make density map like the h129 dataset
 ## display
-fig, axes = plt.subplots(ncols = 1, nrows = 2, figsize = (8,5), sharex = True, gridspec_kw = {"wspace":0, "hspace":0,
-                         "height_ratios": [3,5]})
+fig, axes = plt.subplots(ncols = 1, nrows = 2, figsize = (8,6), sharex = True, gridspec_kw = {"wspace":0, "hspace":0,
+                         "height_ratios": [2.5,5]})
 
 
 #sort inj fractions by primary lob
 sort_density = [counts_per_struct.T[np.where(primary_pool == idx)[0]] for idx in np.unique(primary_pool)]
 sort_density = np.array(list(itertools.chain.from_iterable(sort_density)))
-#now sort sois by # of neurons/density
-sort_sois = np.array(sois)[np.argsort(np.median(sort_density,axis=0))]
-sort_density = sort_density.T[np.argsort(np.median(sort_density,axis=0))][::-1].T
-yaxis = sort_sois
+if sort_descending == True:
+    #now sort sois by # of neurons/density
+    sort_sois = np.array(sois)[np.argsort(np.median(sort_density,axis=0))]
+    sort_density = sort_density.T[np.argsort(np.median(sort_density,axis=0))][::-1].T
+    yaxis = sort_sois
+else: 
+    yaxis = np.flipud(sois)
 sort_brains = [np.asarray(brains)[np.where(primary_pool == idx)[0]] for idx in np.unique(primary_pool)]
 sort_inj = [frac_of_inj_pool[np.where(primary_pool == idx)[0]] for idx in np.unique(primary_pool)]
 sort_brains = list(itertools.chain.from_iterable(sort_brains))
@@ -239,7 +246,7 @@ cmap.set_over(cmap(1.0))
 
 #colormap
 pc = ax.pcolor(show, cmap=cmap, vmin=vmin, vmax=vmax)#, norm=norm)
-cb = plt.colorbar(pc, ax=ax, cmap=cmap, format="%0.1f", shrink=0.6)
+cb = plt.colorbar(pc, ax=ax, cmap=cmap, format="%d", shrink=0.6)
 cb.set_label("# Neurons", fontsize="small", labelpad=5)
 cb.ax.tick_params(labelsize="small")
 cb.ax.set_visible(True)
