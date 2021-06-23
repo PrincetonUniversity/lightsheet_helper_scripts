bdf48d2558510a4db7ae43997b2f2d9851681af4
zahramansoor <zahramansoor@github.com>
2021-06-23 13:39:52 -0400

    updated cell centers script for kelly

--- projects/seagravesk/transform_cells_to_atlas/transform_points_smartspim.py
+++ projects/seagravesk/transform_cells_to_atlas/transform_points_smartspim.py
@@ -8,6 +8,7 @@ Created on Tue Jun  8 16:57:14 2021
 
 from scipy.io import loadmat
 import tifffile as tif, numpy as np, os, matplotlib.pyplot as plt, sys
+import shutil 
 
 def transformix_command_line_call(src, dst, transformfile):
     '''Wrapper Function to call transformix using the commandline, this can be time consuming
@@ -125,17 +126,52 @@ def create_text_file_for_elastix(src, dst):
     sys.stdout.write("...done writing centers\n"); sys.stdout.flush()
     return pth
 
+def unpack_pnts(points_file, dst):
+    """
+    function to take elastix point transform file and return anatomical locations of those points
+    
+    Here elastix uses the xyz convention rather than the zyx numpy convention
+    
+    Inputs
+    -----------
+    points_file = post_transformed file, XYZ
+    
+    Returns
+    -----------
+    dst_fl = path to numpy array, ZYX
+    
+    """   
+    #####inputs 
+    assert type(points_file)==str
+    point_or_index = 'OutputPoint'
+    #get points
+    with open(points_file, "r") as f:                
+        lines=f.readlines()
+        f.close()
+    #####populate post-transformed array of contour centers
+    sys.stdout.write("\n\n{} points detected\n\n".format(len(lines)))
+    arr=np.empty((len(lines), 3))    
+    for i in range(len(lines)):        
+        arr[i,...]=lines[i].split()[lines[i].split().index(point_or_index)+3:lines[i].split().index(point_or_index)+6] #x,y,z            
+    #optional save out of points
+    dst_fl = os.path.join(dst, "posttransformed_zyx_voxels.npy")
+    np.save(dst_fl, np.asarray([(z,y,x) for x,y,z in arr]))    
+    #check to see if any points where found
+    print("output array shape {}".format(arr.shape))
+        
+    return dst_fl
+
 #path to mat file
-mat = "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_14_20_11_f37073_mouse1_20171010/Ex_785_Em_3/corrected/sliding_diff_peak_find_99percentile_test20200806_all_coord_format2.mat"
+mat = "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_15_27_24_f37077_demonstrator_20171011/Ex_785_Em_3/corrected/sliding_diff_peak_find_99percentile_test20200806_all_coord_format2.mat"
 pnts = loadmat(mat)["cell_centers_orig_coord"].astype(int)
 #for resize dimensions
-downsized = "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_14_20_11_f37073_mouse1_20171010/Ex_785_Em_3/reg_downsized_for_atlas.tif"
+downsized = "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_15_27_24_f37077_demonstrator_20171011/Ex_785_Em_3/reg_downsized_for_atlas.tif"
 downsized = tif.imread(downsized) #sagittal
 zd,yd,xd = downsized.shape #sagittal
 #reorient pnts
 pnts_sag = np.array([[xx[2],xx[1],xx[0]] for xx in pnts])
 #get full size dims
-stitched = "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_14_20_11_f37073_mouse1_20171010/Ex_785_Em_3/stitched/RES(7551x5731x3282)"
+stitched = "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_15_27_24_f37077_demonstrator_20171011/Ex_785_Em_3/stitched/RES(7565x5726x2961)/098550/098550_104079"
 y,z = tif.imread(os.path.join(stitched, os.listdir(stitched)[0])).shape #sagittal
 x = len([xx for xx in os.listdir(stitched) if ".tif" in xx]) #sagittal
 f = ((zd/z),(yd/y),(xd/x))
@@ -143,13 +179,13 @@ downsized_pnts_sag = np.array([[xx[0]*f[0],xx[1]*f[1],xx[2]*f[2]] for xx in pnts
 
 #transform
 #make into transformix-friendly text file
-transformed_dst = "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_14_20_11_f37073_mouse1_20171010/Ex_785_Em_3/points"
+transformed_dst = "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_15_27_24_f37077_demonstrator_20171011/Ex_785_Em_3/points"
 if not os.path.exists(transformed_dst): os.mkdir(transformed_dst)
 pretransform_text_file = create_text_file_for_elastix(downsized_pnts_sag, transformed_dst)
-transformfiles = ["/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_14_20_11_f37073_mouse1_20171010/elastix_inverse_transform/TransformParameters.0.txt",                  
-                  "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_14_20_11_f37073_mouse1_20171010/elastix_inverse_transform/TransformParameters.1.txt",                  
-                  "/jukebox/wang/seagravesk/lightsheet/201710_cfos_left_side_only_registration/f37073_mouse1/elastix_inverse_transform/cellch_f37073_mouse1_20171010_790_015na_1hfsds_z5um_1000msec/f37073_mouse1_20171010_488_015na_1hfsds_z5um_150msec_resized_ch00_resampledforelastix_atlas2reg/TransformParameters.0.txt",
-                  "/jukebox/wang/seagravesk/lightsheet/201710_cfos_left_side_only_registration/f37073_mouse1/elastix_inverse_transform/cellch_f37073_mouse1_20171010_790_015na_1hfsds_z5um_1000msec/f37073_mouse1_20171010_488_015na_1hfsds_z5um_150msec_resized_ch00_resampledforelastix_atlas2reg/TransformParameters.1.txt"]
+transformfiles = ["/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_15_27_24_f37077_demonstrator_20171011/elastix_inverse_transform/TransformParameters.0.txt",                  
+                  "/jukebox/LightSheetData/wang-mouse/seagravesk/20200901_15_27_24_f37077_demonstrator_20171011/elastix_inverse_transform/TransformParameters.1.txt",                  
+                  "/jukebox/wang/seagravesk/lightsheet/201710_cfos_left_side_only_registration/f37077_demons/elastix_inverse_transform/cellch_f37077_demonstrator_20171011_790_015na_1hfsds_z5um_1000msec/f37077_demonstrator_20171011_488_015na_1hfsds_z5um_150msec_resized_ch00_resampledforelastix_atlas2reg/TransformParameters.0.txt",
+                  "/jukebox/wang/seagravesk/lightsheet/201710_cfos_left_side_only_registration/f37077_demons/elastix_inverse_transform/cellch_f37077_demonstrator_20171011_790_015na_1hfsds_z5um_1000msec/f37077_demonstrator_20171011_488_015na_1hfsds_z5um_150msec_resized_ch00_resampledforelastix_atlas2reg/TransformParameters.1.txt"]
 #copy over elastix files
 transformfiles = modify_transform_files(transformfiles, transformed_dst) 
 change_transform_parameter_initial_transform(transformfiles[0], 'NoInitialTransform')
