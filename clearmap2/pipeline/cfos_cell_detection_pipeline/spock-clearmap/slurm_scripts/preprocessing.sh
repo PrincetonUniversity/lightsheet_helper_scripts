63a5981ce282066d36289a1810b2f3f2499d5f0d
Austin Hoag <austinthomashoag@gmail.com>
2021-09-13 08:31:14 -0400

    remove stitching/pystripe from main pipeline; update log directory

--- clearmap2/pipeline/cfos_cell_detection_pipeline/spock-clearmap/slurm_scripts/preprocessing.sh
+++ clearmap2/pipeline/cfos_cell_detection_pipeline/spock-clearmap/slurm_scripts/preprocessing.sh
@@ -3,8 +3,8 @@
 #BATCH -p all                # partition (queue)
 #SBATCH -c 12                      # number of cores
 #SBATCH -t 1200                 # time (minutes)
-#SBATCH -o /scratch/zmd/logs/whl_brn_%j.out        # STDOUT #add _%a to see each array job
-#SBATCH -e /scratch/zmd/logs/whl_brn_%j.err        # STDERR #add _%a to see each array job
+#SBATCH -o logs/clearmap_preprocessing_%j.out        # STDOUT #add _%a to see each array job
+#SBATCH -e logs/clearmap_preprocessing_%j.err        # STDERR #add _%a to see each array job
 #SBATCH --contiguous #used to try and get cpu mem to be contigous
 #SBATCH --mem 100000 #100gbs
 
